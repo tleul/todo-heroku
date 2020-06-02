c0a0280acba485035474bdac8a7d5e7bf5aad21d
Tekleselassie Leul <edenlove2019@gmail.com>
2020-06-01 17:45:46 -0700

    fixtwo

--- server.js
+++ server.js
@@ -8,15 +8,22 @@ dbConnect();
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/register', require('./router/register'));
-app.use('/api/signin', require('./router/auth'));
-app.use('/api/addtodo', require('./router/addtodo'));
-
 //Step 3
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
 app.use(morgan('tiny'));
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, x-auth-token',
+	);
+	next();
+});
+app.use('/api/register', require('./router/register'));
+app.use('/api/signin', require('./router/auth'));
+app.use('/api/addtodo', require('./router/addtodo'));
 app.listen(
 	config.port,
 	console.log(' ---Server  $$ connected--- ' + config.port),
