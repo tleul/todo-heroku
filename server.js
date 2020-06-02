717f7306f52041856093183605d2de22c276f8c3
Tekleselassie Leul <edenlove2019@gmail.com>
2020-06-01 17:35:44 -0700

    fixtwo

--- server.js
+++ server.js
@@ -1,28 +1,12 @@
 const express = require('express');
 const config = require('./config/config');
 const dbConnect = require('./config/db');
-
-// import data from './data.json';
-
-// import UserSchema from '../model/User';
+const morgan = require('morgan');
 const app = express();
-
-app.use(express.static('public'));
 
 dbConnect();
 app.use(express.json({ extended: false }));
-//Test
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, x-auth-token',
-	);
-	next();
-});
-app.get('/', (req, res) => {
-	res.send('Connected');
-});
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/register', require('./router/register'));
 app.use('/api/signin', require('./router/auth'));
@@ -32,6 +16,7 @@ app.use('/api/addtodo', require('./router/addtodo'));
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
+app.use(morgan('tiny'));
 app.listen(
 	config.port,
 	console.log(' ---Server  $$ connected--- ' + config.port),
